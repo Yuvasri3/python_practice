15a658de8a4b33206172d510679d3c7ffe360b0f
Yuvasri3 <yuvalily5@gmail.com>
2025-08-04 10:04:34 +0530

    version2

---
+++ python_file.py
@@ -0,0 +1,20 @@
+# A Simple User Input Age Calculator!!!
+
+def age():
+  current_yr = int(input("Enter the current Year : "))
+  current_month = int(input("Enter the current month : "))
+
+  yr_birth = int(input("Enter your Birth Year : "))
+  month_birth = int(input("Enter your Birth month : "))
+
+  year = current_yr-yr_birth
+  month = current_month-month_birth
+
+  if month < 0:                                   # if ur bday month not yet came! this will adjust ur DOB.
+    year = year - 1
+    month = month + 12
+
+  print(f'your are {year} years & {month} month old')
+
+
+age()
